8c6c8e4e7dc76ee5c86918dc0679f3ef424d6c12
developerkangaroo <rarscript0@gmail.com>
2025-06-27 10:32:57 +0200

    Added materials

--- main.cpp
+++ main.cpp
@@ -239,10 +239,18 @@ int main() {
 
 		//make cube matrix 
 		cubeShader.use();
-		cubeShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 		cubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-		cubeShader.setVec3("lightPos", lightPos);
+		cubeShader.setVec3("light.position", lightPos);
 		cubeShader.setVec3("viewPos", camera.Position);
+
+		cubeShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		cubeShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		cubeShader.setVec3("material.specular", 0.5, 0.5f, 0.5f);
+		cubeShader.setFloat("material.shininess", 32.0f);
+
+		cubeShader.setVec3("light.ambient", 0.8f, 0.8f, 0.8f);
+		cubeShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		cubeShader.setVec3("light.shininess", 1.0f, 1.0f, 1.0f);
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
